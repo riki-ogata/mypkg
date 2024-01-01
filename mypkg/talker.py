4d78c9bfce10f26de6f89c30bc6b9122b6541e70
rikiogata <s20c1705pv@s.chibakoudai.jp>
2024-01-01 21:38:37 +0900

    add code

---
+++ mypkg/talker.py
@@ -0,0 +1,29 @@
+#SPDX-FileCopyrightText: 2023  Ogata Riki
+#SPDX-License-Identifier: BSD-3-Clause
+
+import rclpy                     #ROS 2のクライアントのためのライブラリ
+ from rclpy.node import Node      #ノードを実装するためのNodeクラス（クラスは第10回で）
+from std_msgs.msg import Int16   #通信の型（16ビットの符号付き整数）
+
+class Talker():
+    def __init__(self, node):
+        self.pub = node.create_publisher(Int16, "countup", 10)
+        self.n = 0
+        node.create_timer(0.5, self.cb)
+
+
+    def cb(self):
+        msg = Int16()
+        msg.data = self.n
+        self.pub.publish(msg)
+        self.n += 1
+
+def main():
+    rclpy.init()
+    node = Node("talker")
+    talker = Talker(node)
+    rclpy.spin(node)
+
+if __name__ == '__main__':
+    main()
+        
